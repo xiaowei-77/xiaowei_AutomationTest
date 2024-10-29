4e271bd083297f0e85a3b8acdb9ae32372604df0
xiaowei-77 <18892080677@163.com>
2024-10-29 16:26:08 +0800

    提交基本框架结构

---
+++ testcase/conftest.py
@@ -0,0 +1,7 @@
+import pytest
+
+
+@pytest.fixture
+def print_text():
+    text1 = '1354867864787'
+    return text1
---
+++ testcase/test_1.py
@@ -0,0 +1,23 @@
+import pytest
+
+
+class Test:
+
+    def test12(self):
+        print(12 * 15)
+
+    @pytest.mark.usefixtures
+    def test_fibon(self, print_text):
+        fib_list = []
+        for i in range(10):
+            if i == 0 or i == 1:
+                fib_list.append(1)
+            else:
+                fib_list.append(fib_list[i - 2] + fib_list[i - 1])
+        print(fib_list, '\t', print_text)
+
+
+if __name__ == '__main__':
+    t = Test()
+    t.test12()
+    t.fibon()
